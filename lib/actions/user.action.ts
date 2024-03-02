b3ce44defe0251adb8383cf7053531b3a5d7dd26
abdelrahman830a <abdelrahman.1220005@stemalex.moe.edu.eg>
2024-03-02 18:34:43 +0200

    implemented the Community Page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../database/user.model";
 import { connectToDatabase } from "../mongoose"
-import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { CreateUserParams, DeleteUserParams, GetAllUsersParams, UpdateUserParams } from "./shared.types";
 import Question from "../database/question.model";
 
 export async function getUserById(params: any) {
@@ -15,6 +15,21 @@ export async function getUserById(params: any) {
         const user = await User.findOne({ clerkId: userId });
 
         return user;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function getAllUsers(params: GetAllUsersParams) {
+    try {
+        connectToDatabase();
+
+        // const { page = 1, pageSize = 10, searchQuery, filter } = params;
+
+        const users = await User.find({}).sort({ createdAt: -1 });
+
+        return { users };
     } catch (error) {
         console.log(error);
         throw error;
